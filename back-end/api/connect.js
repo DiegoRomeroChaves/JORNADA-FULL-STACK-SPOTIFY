2f3694cb6270cfd73e24152db0bb47912153d46e
Diego_Chaves <diegoromeroribeirochaves@gmail.com>
2025-02-20 00:32:03 -0300

    Projeto full stack spotify

---
+++ back-end/api/connect.js
@@ -0,0 +1,16 @@
+
+// java script assincrono
+// await async
+// fullfilled
+import { MongoClient } from "mongodb";
+
+const URI = 'mongodb+srv://diegoromeroribeirochaves:Rmro4512@cluster0.u7ecd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const client = new MongoClient(URI);
+
+export const db = client.db("spotifyAula");
+
+// const songCollection = await db.collection("songs").find({}).toArray();
+
+
+// console.log(songCollection);
